62495d4f8639ec88a2403552283172a00a83a0a7
yash <yash.tambat@endlos.tech>
2025-06-26 10:26:10 +0530

    firstcommit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import authRoutes from './routes/authRoutes.js';
+// import booksRoutes from './routes/booksRoutes.js';
+import { connectDB } from './lib/db.js';
+import job from './lib/cron.js';
+
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+job.start();         // for physical phone make it open and for simulator make it close
+app.use(express.json());
+app.use(cors());
+app.use("/api/auth" , authRoutes)
+// app.use("/api/books" , booksRoutes)
+app.listen(PORT , ()=>{
+  console.log(`Server is running on ${PORT}`)
+  connectDB();
+})
